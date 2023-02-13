c220a254c75a66429f59e74e0d9128130747bca7
sungjae <sungjae0712@gmail.com>
2023-02-13 20:14:06 +0900

    02-13 편지_120898

---
+++ src/프로그래머스/num_120898.java
@@ -0,0 +1,19 @@
+package 프로그래머스;
+
+public class num_120898 {
+    public static void main(String[] args) {
+
+        int answer = 0;
+        String message = "happy birthday!";
+
+        answer = message.length() * 2;
+
+        System.out.println(answer);
+
+    }
+
+}
+
+//message	            result
+//"happy birthday!"	    30
+//"I love you~"	        22
