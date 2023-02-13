ad750990b90ad79ec03a2e787e3546887e9f3e58
sungjae <sjhj825@naver.com>
2023-02-13 21:45:34 +0900

    02-13 배열의 유사도_120903

---
+++ src/프로그래머스/num_120903.java
@@ -0,0 +1,23 @@
+package 프로그래머스;
+
+public class num_120903 {
+    public static void main(String[] args) {
+
+        String[] s1 = {"a", "b", "c"};
+        String[] s2 = {"com", "b", "d", "p", "c"};
+
+        int answer = 0;
+
+        int l1 = s1.length;
+        int l2 = s2.length;
+
+        for(int i = 0; i < l1; i++) {
+            for (int j = 0; j < l2; j++) {
+                if(s1[i] == s2[j]) {
+                    answer++;
+                }
+            }
+        }
+        System.out.println(answer);
+    }
+}
