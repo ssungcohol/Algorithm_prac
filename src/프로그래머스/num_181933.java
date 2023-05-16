1d300c6bff60f1018637a8bc55852112459a41bd
sungjae <sjhj825@naver.com>
2023-05-16 16:37:44 +0900

    05-16 flag에 따라 다른 값 반환_181933

---
+++ src/프로그래머스/num_181933.java
@@ -0,0 +1,19 @@
+package 프로그래머스;
+
+public class num_181933 {
+	public static void main(String[] args) {
+		int a = -4;
+		int b = 7;
+		boolean flag = false;
+		int answer = 0;
+
+		if (flag == true) {
+			answer = a + b;
+		} else {
+			answer = a - b;
+		}
+
+		System.out.println(answer);
+	}
+
+}
