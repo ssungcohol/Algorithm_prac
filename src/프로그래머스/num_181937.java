652bb5a6c4628551210fe90beda0d35f3d558e5d
sungjae <sjhj825@naver.com>
2023-05-16 16:42:11 +0900

    05-16 n의 배수_181937

---
+++ src/프로그래머스/num_181937.java
@@ -0,0 +1,16 @@
+package 프로그래머스;
+
+public class num_181937 {
+	public static void main(String[] args) {
+		int num = 34;
+		int n = 3;
+
+		int answer = 0;
+
+		if (num%n == 0) {
+			answer = 1;
+		}
+
+		System.out.println(answer);
+	}
+}
