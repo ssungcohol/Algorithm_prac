4d5a85e77b14b97796a348926054b27572ab37d0
sungjae <sjhj825@naver.com>
2023-05-17 16:52:32 +0900

    05-17 문자열 앞의 n글자_181907

---
+++ src/프로그래머스/num_181907.java
@@ -0,0 +1,12 @@
+package 프로그래머스;
+
+public class num_181907 {
+	public static void main(String[] args) {
+
+		String my_string = "ProgrammerS123";
+		int n = 11;
+
+		System.out.println(my_string.substring(0,n));
+
+	}
+}
