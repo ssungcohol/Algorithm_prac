4cf3cbdcc8dc45b28ab0e2ff4f5f50efb961bfb9
sungjae <sjhj825@naver.com>
2023-05-17 17:13:54 +0900

    05-17 문자열을 정수로 변환하기_181848

---
+++ src/프로그래머스/num_181848.java
@@ -0,0 +1,13 @@
+package 프로그래머스;
+
+public class num_181848 {
+	public static void main (String[] args) {
+
+		String n_str = "10";
+
+		int answer = Integer.parseInt(n_str);
+
+		System.out.println(answer);
+
+	}
+}
