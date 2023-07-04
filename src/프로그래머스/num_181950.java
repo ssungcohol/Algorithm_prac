fa1087349a784093c662f7440882cfab52acf5dd
sungjae <sjhj825@naver.com>
2023-07-04 18:28:47 +0900

    07-04 문자열 반복해서 출력하기_181950

---
+++ src/프로그래머스/num_181950.java
@@ -0,0 +1,19 @@
+package 프로그래머스;
+
+public class num_181950 {
+	public static void main(String[] args) {
+
+		String str = "string";
+
+		int n = 5;
+
+		String answer = "";
+
+		for(int i = 1; i <= n; i++) {
+			answer += str;
+		}
+
+		System.out.println(answer);
+
+	}
+}
