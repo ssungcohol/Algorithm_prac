a6d5cc2a7f47c72c4056bf050ffed70f08208d9f
sungjae <sjhj825@naver.com>
2023-07-28 18:21:08 +0900

    07-28  문자 개수 세기_181902

---
+++ src/프로그래머스/num_181902.java
@@ -0,0 +1,122 @@
+package 프로그래머스;
+
+import java.util.Arrays;
+
+public class num_181902 {
+	public static void main(String[] args) {
+
+		String my_string = "Programmers";
+
+		int[] answer = new int[52];
+
+		for(int i = 0; i < my_string.length(); i++) {
+			if(my_string.charAt(i) == 65) {
+				answer[0]++;
+			} else if (my_string.charAt(i) == 66) {
+				answer[1]++;
+			} else if (my_string.charAt(i) == 67) {
+				answer[2]++;
+			} else if (my_string.charAt(i) == 68) {
+				answer[3]++;
+			} else if (my_string.charAt(i) == 69) {
+				answer[4]++;
+			} else if (my_string.charAt(i) == 70) {
+				answer[5]++;
+			} else if (my_string.charAt(i) == 71) {
+				answer[6]++;
+			} else if (my_string.charAt(i) == 72) {
+				answer[7]++;
+			} else if (my_string.charAt(i) == 73) {
+				answer[8]++;
+			} else if (my_string.charAt(i) == 74) {
+				answer[9]++;
+			} else if (my_string.charAt(i) == 75) {
+				answer[10]++;
+			} else if (my_string.charAt(i) == 76) {
+				answer[11]++;
+			} else if (my_string.charAt(i) == 77) {
+				answer[12]++;
+			} else if (my_string.charAt(i) == 78) {
+				answer[13]++;
+			} else if (my_string.charAt(i) == 79) {
+				answer[14]++;
+			} else if (my_string.charAt(i) == 80) {
+				answer[15]++;
+			} else if (my_string.charAt(i) == 81) {
+				answer[16]++;
+			} else if (my_string.charAt(i) == 82) {
+				answer[17]++;
+			} else if (my_string.charAt(i) == 83) {
+				answer[18]++;
+			} else if (my_string.charAt(i) == 84) {
+				answer[19]++;
+			} else if (my_string.charAt(i) == 85) {
+				answer[20]++;
+			} else if (my_string.charAt(i) == 86) {
+				answer[21]++;
+			} else if (my_string.charAt(i) == 87) {
+				answer[22]++;
+			} else if (my_string.charAt(i) == 88) {
+				answer[23]++;
+			} else if (my_string.charAt(i) == 89) {
+				answer[24]++;
+			} else if (my_string.charAt(i) == 90) {
+				answer[25]++;
+			} else if (my_string.charAt(i) == 97) {
+				answer[26]++;
+			} else if (my_string.charAt(i) == 98) {
+				answer[27]++;
+			} else if (my_string.charAt(i) == 99) {
+				answer[28]++;
+			} else if (my_string.charAt(i) == 100) {
+				answer[29]++;
+			} else if (my_string.charAt(i) == 101) {
+				answer[30]++;
+			} else if (my_string.charAt(i) == 102) {
+				answer[31]++;
+			} else if (my_string.charAt(i) == 103) {
+				answer[32]++;
+			} else if (my_string.charAt(i) == 104) {
+				answer[33]++;
+			} else if (my_string.charAt(i) == 105) {
+				answer[34]++;
+			} else if (my_string.charAt(i) == 106) {
+				answer[35]++;
+			} else if (my_string.charAt(i) == 107) {
+				answer[36]++;
+			} else if (my_string.charAt(i) == 108) {
+				answer[37]++;
+			} else if (my_string.charAt(i) == 109) {
+				answer[38]++;
+			} else if (my_string.charAt(i) == 110) {
+				answer[39]++;
+			} else if (my_string.charAt(i) == 111) {
+				answer[40]++;
+			} else if (my_string.charAt(i) == 112) {
+				answer[41]++;
+			} else if (my_string.charAt(i) == 113) {
+				answer[42]++;
+			} else if (my_string.charAt(i) == 114) {
+				answer[43]++;
+			} else if (my_string.charAt(i) == 115) {
+				answer[44]++;
+			} else if (my_string.charAt(i) == 116) {
+				answer[45]++;
+			} else if (my_string.charAt(i) == 117) {
+				answer[46]++;
+			} else if (my_string.charAt(i) == 118) {
+				answer[47]++;
+			} else if (my_string.charAt(i) == 119) {
+				answer[48]++;
+			} else if (my_string.charAt(i) == 120) {
+				answer[49]++;
+			} else if (my_string.charAt(i) == 121) {
+				answer[50]++;
+			} else if (my_string.charAt(i) == 122) {
+				answer[51]++;
+			}
+		}
+
+		System.out.println(Arrays.toString(answer));
+	}
+}
