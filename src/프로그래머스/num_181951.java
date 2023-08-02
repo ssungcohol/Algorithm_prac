12cebb4d26d69d7fa7f5b9bef8c69206f3fb65c5
sungjae <sjhj825@naver.com>
2023-08-02 16:31:11 +0900

    08-02  a와 b 출력하기_181951

---
+++ src/프로그래머스/num_181951.java
@@ -0,0 +1,17 @@
+package 프로그래머스;
+
+import java.util.Scanner;
+
+public class num_181951 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+
+	}
+}
